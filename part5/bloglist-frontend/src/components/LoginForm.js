24d048dbcaa19acc11cfa96d84b85c9c5f67d9f9
tripplen23 <ducbinhnguyen23@gmail.com>
2023-06-21 21:48:20 +0700

    Update 5.12: Add prop-types and ESLint

--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const LoginForm = ({ handleLogin }) => {
   const [username, setUsername] = useState("");
@@ -34,6 +35,10 @@ const LoginForm = ({ handleLogin }) => {
       <button type="submit">Login</button>
     </form>
   );
+};
+
+LoginForm.propTypes = {
+  handleLogin: PropTypes.func.isRequired,
 };
 
 export default LoginForm;
