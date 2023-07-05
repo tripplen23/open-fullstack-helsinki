530f07884a5a2d9f214419a4a552bd6e4a72c33d
tripplen23 <ducbinhnguyen23@gmail.com>
2023-07-05 18:25:00 +0700

    5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -17,4 +17,23 @@ describe('Blog app', function () {
   it('Login form is shown', function () {
     cy.get('.loginForm').should('exist');
   });
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('Binh');
+      cy.get('#password').type('123123');
+      cy.get('#login-btn').click();
+      cy.contains('Duc Binh logged in');
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('Binh');
+      cy.get('#password').type('123456');
+      cy.get('#login-btn').click();
+
+      cy.get('.error')
+        .should('contain', 'invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid');
+    });
+  });
 });
--- part5/bloglist-frontend/src/components/Notification.js
+++ part5/bloglist-frontend/src/components/Notification.js
@@ -1,7 +1,7 @@
 const Notification = ({ message }) => {
   if (message === null) return null;
 
-  if (message.includes('error')) {
+  if (message.includes('Error')) {
     return <div className='error'>{message.substring(5)}</div>;
   }
 
