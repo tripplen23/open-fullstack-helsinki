2d089530ce9882f362ffd9b8abc18ebbc86c377f
tripplen23 <ducbinhnguyen23@gmail.com>
2023-07-19 21:28:23 +0700

    Update 6.16

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import anecdoteService from "../services/anecdotes";
 
 const anecdoteSlice = createSlice({
   name: "anecdotes",
@@ -29,6 +30,13 @@ const anecdoteSlice = createSlice({
     },
   },
 });
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
 
 export const { addVote, addAnecdote, appendAnecdote, setAnecdotes } =
   anecdoteSlice.actions;
