577ed82776026073141acbe63c3d4783f399148a
tripplen63 <tripplen63@gmail.com>
2023-09-28 16:27:07 +0300

    Update 8.4

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -93,7 +93,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
   type Book {
@@ -115,7 +115,14 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books;
+      }
+      const byAuthor = (book) =>
+        args.author === book.author ? book.author : !book.author;
+      return books.filter(byAuthor);
+    },
     allAuthors: () => authors,
   },
   Author: {
