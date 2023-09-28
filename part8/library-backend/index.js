feac6aca2b1e488fec16c1dc5640ff60e8411615
tripplen63 <tripplen63@gmail.com>
2023-09-28 18:13:32 +0300

    Update: 8.7

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -118,6 +118,11 @@ const typeDefs = `
       author: String!
       genres: [String!]!
     ) : Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ) : Author
   }
 `;
 
@@ -183,6 +188,19 @@ const resolvers = {
       const book = { ...args, id: uuid() };
       books = books.concat(book);
       return book;
+    },
+
+    editAuthor: (root, args) => {
+      const author = authors.find((author) => author.name === args.name);
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((author) =>
+        author.name === args.name ? updatedAuthor : author
+      );
+      return updatedAuthor;
     },
   },
 };
