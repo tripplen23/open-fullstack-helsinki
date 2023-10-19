c1b8e6c0386d430b57cf060555dbd8e0c3847bfc
tripplen63 <tripplen63@gmail.com>
2023-10-19 23:02:03 +0300

    Fix 9.1

--- part9/9.1-9.7/bmiCalculator.ts
+++ part9/9.1-9.7/bmiCalculator.ts
@@ -9,7 +9,7 @@ const calculateBmi = (height: number, weight: number): string => {
   } else if (bmi >= 17.0 && bmi <= 18.4) {
     return "Underweight - Mild thinness";
   } else if (bmi >= 18.5 && bmi <= 24.9) {
-    return "Normal range";
+    return "Normal (healthy weight)";
   } else if (bmi >= 25.0 && bmi <= 29.9) {
     return "Overweight - Pre-obese";
   } else if (bmi >= 30.0 && bmi <= 34.9) {
